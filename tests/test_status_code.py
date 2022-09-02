f1c0493593307c63eee2036e2c531c603a39dbb5
akvarpop <popravk@gmail.com>
2022-09-02 15:31:46 +0300

    Add allure

--- tests/test_status_code.py
+++ tests/test_status_code.py
@@ -2,6 +2,12 @@
 Тестуемо усі сторінки сайту:https://www.symphony-solutions.eu/ на статус код 200
 We test all pages of the site: https://www.symphony-solutions.eu/ for status code 200
 """
+"""
+Для запуску усіх тестів в терміналі:pytest -v -s C:\Test_status_code_symphony_com\tests
+Для запуску тестів в файл з збереженням файлу Allure: 
+pytest -v -s C:\Test_status_code_symphony_eu --alluredir=allure_ss_eu
+Збгенерувати посилання на звіт по тестах: allure serve allure_ss_eu
+"""
 import pytest
 import requests
 from configuration import SERVICE_URL, ALL_POST_URL, ALL_MAIN_PAGE_URL, \
